15d311fc419a3e010252051a2a3facc7b40d7179
Ian McEwen <mian@cyverse.org>
2016-08-30 14:12:10 -0700

    Version 2.8.0 (+switch to org.cyverse)

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject org.iplantc/clj-icat-direct "5.2.8.0"
+(defproject org.cyverse/clj-icat-direct "2.8.0"
   :description "A Clojure library for accessing the iRODS ICAT database directly."
-  :url "https://github.com/iPlantCollaborativeOpenSource/DE"
+  :url "https://github.com/cyverse-de/clj-icat-direct"
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.mchange/c3p0 "0.9.5.1"]
                  [korma "0.4.2"
