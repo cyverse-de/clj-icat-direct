baa6c4950c3a7cc4c9ab04b11d32ce45d50b55f6
Ian McEwen <mian@cyverse.org>
2017-02-13 10:55:01 -0700

    Version 2.8.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/clj-icat-direct "2.8.1"
+(defproject org.cyverse/clj-icat-direct "2.8.2-SNAPSHOT"
   :description "A Clojure library for accessing the iRODS ICAT database directly."
   :url "https://github.com/cyverse-de/clj-icat-direct"
   :license {:name "BSD Standard License"
